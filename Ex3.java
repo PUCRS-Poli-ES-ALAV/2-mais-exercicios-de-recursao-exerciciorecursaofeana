a32f7fc7309d683b56d5c468fcdabf27029ef563
FeFeFarias05 <fernanda.farias05@hotmail.com>
2024-08-12 20:05:31 -0300

    Exercício 3 completo!

--- Ex3.java
+++ Ex3.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Ex3{
-    static int fibonacci(int n){
-        return 0;
+    static int fibonacci(int n) throws Exception{
+        if(n <= 1)
+        {
+            return n;
+        }
+        if(n < 0){
+             throw new Exception("Número inválido");
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
-    public static void main(String[] args) {
-        System.out.println("O Fibonacci de " );
+    public static void main(String[] args) throws Exception {
+    Scanner t = new Scanner(System.in);
+    System.out.println("Digite a posição que deseja seu Fibonacci");
+    int num = t.nextInt();
+        System.out.println("O Fibonacci de " + num + " é " + fibonacci(num) );
     }
 }
