c976906f2e734bac933382a15a020dace303f5a9
FeFeFarias05 <fernanda.farias05@hotmail.com>
2024-10-16 19:43:16 -0300

    concertando erros

--- Ex5.java
+++ Ex5.java
@@ -5,17 +5,19 @@ public class Ex5{
         if(s.equals("")){
             return true;
         }
-        if(s == null){
-            return true;
+        if(s.equals(null)){
+            return false;
         }
+
         if(s.length() == 1){
             return true;
         }
 
-        if(s.substring(0).equals(s.substring(s.length()-1))){
-            return isPal(s.substring(1,s.substring(s.length()-1)));
+        if((s.charAt(0) != s.charAt(s.length()-1))){
+            return false;
         }
         
+        return isPal(s.substring(1,s.length()-1));
     }
 
     public static void main(String[] args) {
